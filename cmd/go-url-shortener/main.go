fcab8b089f9e0ae6925332b30bbece77c242faaa
Alexander Gnatyuk <agnatyuk@croc.ru>
2023-11-12 03:19:04 +0300

    main: add deps, provide app entrypoint

--- cmd/go-url-shortener/main.go
+++ cmd/go-url-shortener/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"flag"
+	"net/http"
+
+	"github.com/cactusfriday/go-url-shortener/internal/api"
+	"github.com/cactusfriday/go-url-shortener/internal/common"
+)
+
 func main() {
+	var storageType string
+	flag.StringVar(&storageType, "s", "ram", "Storage type to use <sql || ram>")
+	flag.Parse()
+
+	common.InitConfig(storageType)
+
+	server := api.NewServer()
+
+	http.ListenAndServe(":8080", server)
 }
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/cactusfriday/go-url-shortener
+
+go 1.21
+
+require github.com/lib/pq v1.10.9
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/vrischmann/envconfig v1.3.0 // indirect
+)
